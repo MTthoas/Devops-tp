b10dea77c77e08222ce037dab52328cabcc1d83c
MtThoas <matthias.pecquery@gmail.com>
2023-12-19 12:44:40 +0100

    test

--- src/app.ts
+++ src/app.ts
@@ -8,6 +8,12 @@ app.use(express.json());
 
 app.get('/user', UserController.getUser);
 
-app.listen(port, () => {
-  console.log(`Serveur démarré sur http://localhost:${port}`);
-});
+// Exportez l'application Express pour l'utiliser dans d'autres fichiers, comme vos tests
+export default app;
+
+// Séparez la logique de démarrage du serveur
+if (require.main === module) {
+    app.listen(port, () => {
+        console.log(`Serveur démarré sur http://localhost:${port}`);
+    });
+}
